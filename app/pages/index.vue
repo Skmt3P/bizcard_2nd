2e956cd7ef7ba407d99b69a2a54d4117908a6324
Sakamoto-san <skmt3p@gmail.com>
2019-02-03 19:36:03 +0900

    beta ver.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content-center">
+  <div class="main content-center" >
     <!-- <logo/>
       <h1 class="title">
         bizcard_2nd
@@ -16,28 +16,51 @@
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey">GitHub</a> -->
-    <div class="container w-full max-h-screen">
+    <div class="container">
       <section
         id="card" 
-        class="flex rounded overflow-hidden shadow-lg">
+        class="flex rounded shadow-lg overflow-scroll bg-image">
         <div 
           id="line1" 
           class="sm:flex md:inline-flex lg:inline-flex xl:inline-flex">
           <div class="flex-box">
             <img 
               id="img"
-              class="content"
-              src="https://tailwindcss.com/img/card-top.jpg" 
-              alt="Sunset in the mountains">
+              class="content overflow-hidden"
+              src="~/assets/image/icon.png" 
+              alt="Sakamoto-san's photo">
           </div>
-          <div class="flex-box">
-            <div class="content font-bold text-xl mb-2">The Coldest Sunset</div>
+          <div class="flex-box sm:max-w-3/4 md:max-w-1/2 lg:max-w-1/2 xl:max-w-1/2">
+            <h1 class="overflow-scroll">Sakamoto-san / Skmt3P's BizCard</h1>
+            <img 
+              id="img"
+              class="content overflow-hidden"
+              src="~/assets/image/sign.png" 
+              alt="Sakamoto-san's photo"
+              style="width:80%; height:80%">
             <p class="content text-grey-darker text-base">
-              Lorem ipsum dolor sit amet, <br>consectetur adipisicing elit. <br>Voluptatibus quia, nulla! Maiores et perferendis eaque, <br>exercitationem praesentium nihil.
+              <br>I'm a Frontend Engineer from Japan.<br>
+              My output is <a href="https://scrapbox.io/skmt3p/">HERE</a>!!<br>
+              Contact me by these Social Media!!<br><br>
             </p>
-            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-            <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+            <div class="contenrt">
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#Vue.js</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#nuxt</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#Angular</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#extjs</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#php</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#Python</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#Docker</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#Firebase</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#CircleCI</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#git</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#SQL</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#MongoDB</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#RESTfulAPI</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#sketch</span>
+              <span class="tags inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#SAP ERP</span>
+            </div>
+            <p><br></p>
           </div>
         </div>
       </section>
@@ -62,14 +85,26 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-#card {
-  margin: 10vh;
-  height: 90vh;
+html {
+  background-image: url('../assets/image/bg-wood.png');
+  /* background-color: red; */
+}
+
+section {
+  /* background-image: ('~/assets/image/bg-wood.png'); */
+  background-color: white;
+}
+
+.main {
+  height: 100%;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 5vh;
 }
 
 .container {
-  min-height: 100%;
-  min-width: 100%;
+  min-height: 90%;
+  min-width: 90%;
   display: center;
   justify-content: center;
   align-items: center;
@@ -77,13 +112,34 @@ export default {
   margin: auto;
 }
 
+#card {
+  margin: 0;
+  height: auto;
+  max-height: 90vh;
+  width: auto;
+  max-width: 90vw;
+  display: flex;
+  justify-content: space-between;
+}
+
+#line1 {
+  width: 95%;
+}
+
 .flex-box {
-  margin: 0.5em;
+  margin: auto;
+  min-width: 50%;
 }
 
 .content {
   margin: auto;
-  position: center;
+}
+
+.tags {
+  margin: 1%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 
 /* .title {
